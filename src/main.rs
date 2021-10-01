15ea180e2c6fe7171aa764781229d1b803e27899
Wojciech Kępka <wojciech@wkepka.dev>
2021-10-01 16:38:05 +0200

    Rename lib to `libepick` to avoid collision that causes build troubles

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use eframe::NativeOptions;
 
 fn main() {
-    eframe::run_native(Box::new(epick::Epick::default()), NativeOptions::default())
+    eframe::run_native(
+        Box::new(libepick::Epick::default()),
+        NativeOptions::default(),
+    )
 }
