6edf7cae898248feebd346255b1c97d15054cd9b
Wojciech Kępka <wojciech@wkepka.dev>
2021-10-07 05:43:04 +0200

    Update wasm scripts

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 set -eu
 
-# ./setup_web.sh # <- call this first!
-
 FOLDER_NAME=${PWD##*/}
 CRATE_NAME=$FOLDER_NAME # assume crate name is the same as the folder name
 CRATE_NAME_SNAKE_CASE="${CRATE_NAME//-/_}" # for those who name crates with-kebab-case
+
+BUILD=release
+TARGET=wasm32-unknown-unknown
+CARGO_BUILD_FLAGS="-p ${CRATE_NAME} \
+                  --lib \
+                  --target ${TARGET}"
+
+if [[ $BUILD =~ release ]]
+then
+    CARGO_BUILD_FLAGS="--release ${CARGO_BUILD_FLAGS}"
+fi
+
+OUT_DIR=docs
+OUT_FILE="${CRATE_NAME_SNAKE_CASE}.wasm"
+WASM_FLAGS="--out-dir ${OUT_DIR} \
+            --target no-modules  \
+            --no-typescript      \
+            --omit-imports"
+
+
 
 # This is required to enable the web_sys clipboard API which egui_web uses
 # https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.Clipboard.html
 # https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html
 export RUSTFLAGS=--cfg=web_sys_unstable_apis
 
+
 # Clear output from old stuff:
-rm -f docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm
+rm -f ${OUT_DIR}/$OUT_FILE
+
 
 echo "Building rust…"
-BUILD=release
-cargo build --release -p ${CRATE_NAME} --lib --target wasm32-unknown-unknown
+cargo build $CARGO_BUILD_FLAGS
 
 echo "Generating JS bindings for wasm…"
-TARGET_NAME="${CRATE_NAME_SNAKE_CASE}.wasm"
-wasm-bindgen "target/wasm32-unknown-unknown/${BUILD}/${TARGET_NAME}" \
-  --out-dir docs --target no-modules --no-typescript --omit-imports
+wasm-bindgen "target/${TARGET}/${BUILD}/${OUT_FILE}" $WASM_FLAGS
 
-# to get wasm-opt:  apt/brew/dnf install binaryen
-# echo "Optimizing wasm…"
-# wasm-opt docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm -O2 --fast-math -o docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm # add -g to get debug symbols
-
-echo "Finished: docs/${CRATE_NAME_SNAKE_CASE}.wasm"
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Linux, ex: Fedora
-  xdg-open http://localhost:8080/index.html
-elif [[ "$OSTYPE" == "msys" ]]; then
-  # Windows
-  start http://localhost:8080/index.html
-else
-  # Darwin/MacOS, or something else
-  open http://localhost:8080/index.html
-fi
+echo "Finished: ${OUT_DIR}/${OUT_FILE//.wasm/_bg.wasm}"
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -eu
 
-# Starts a local web-server that serves the contents of the `doc/` folder,
-# which is the folder to where the web version is compiled.
+OUT_DIR=docs
+LISTEN_ADDR=127.0.0.1
+LISTEN_PORT=8080
 
 cargo install basic-http-server
 
-echo "open http://localhost:8080"
+echo "open http://${LISTEN_ADDR}:${LISTEN_PORT}"
 
-(cd docs && basic-http-server --addr 127.0.0.1:8080 .)
-# (cd docs && python3 -m http.server 8080 --bind 127.0.0.1)
+cd $OUT_DIR && \
+    basic-http-server --addr $LISTEN_ADDR:$LISTEN_PORT .
