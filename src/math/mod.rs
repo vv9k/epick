db3b32d8d5c6f1d8cf02c27e4067b56fa6245a67
Wojciech Kępka <wojciech@wkepka.dev>
2021-10-17 16:12:35 +0200

    Add `math::wrap_f32`

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -3,3 +3,24 @@ mod matrix3x3;
 
 pub use matrix1x3::Matrix1x3;
 pub use matrix3x3::Matrix3;
+
+#[inline(always)]
+pub fn wrap_f32(num: f32) -> f32 {
+    (num + 1.).fract()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::wrap_f32;
+
+    #[test]
+    fn it_wraps() {
+        assert_eq!(wrap_f32(0.), 0.);
+        assert_eq!(wrap_f32(-1. / 8.), 7. / 8.);
+        assert_eq!(wrap_f32(-1. / 2.), 1. / 2.);
+        assert_eq!(wrap_f32(-1.), 0.);
+        assert_eq!(wrap_f32(1. / 8.), 1. / 8.);
+        assert_eq!(wrap_f32(1. / 2.), 1. / 2.);
+        assert_eq!(wrap_f32(1.), 0.);
+    }
+}
