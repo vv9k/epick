1e29baa808399d1ba6592e426dd16dc0b8cdd994
Wojciech Kępka <wojciech@wkepka.dev>
2021-10-17 16:29:39 +0200

    Add `math::eq_f32`

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -9,18 +9,31 @@ pub fn wrap_f32(num: f32) -> f32 {
     (num + 1.).fract()
 }
 
+#[inline(always)]
+pub fn eq_f32(lhs: f32, rhs: f32) -> bool {
+    (lhs - rhs).abs() < f32::EPSILON
+}
+
 #[cfg(test)]
 mod tests {
-    use super::wrap_f32;
+    use super::{eq_f32, wrap_f32};
 
     #[test]
     fn it_wraps() {
-        assert_eq!(wrap_f32(0.), 0.);
-        assert_eq!(wrap_f32(-1. / 8.), 7. / 8.);
-        assert_eq!(wrap_f32(-1. / 2.), 1. / 2.);
-        assert_eq!(wrap_f32(-1.), 0.);
-        assert_eq!(wrap_f32(1. / 8.), 1. / 8.);
-        assert_eq!(wrap_f32(1. / 2.), 1. / 2.);
-        assert_eq!(wrap_f32(1.), 0.);
+        assert!(eq_f32(wrap_f32(0.0f32), 0.0f32));
+        assert!(eq_f32(wrap_f32(-1. / 8.), 7. / 8.));
+        assert!(eq_f32(wrap_f32(-1. / 2.), 1. / 2.));
+        assert!(eq_f32(wrap_f32(-1.), 0.));
+        assert!(eq_f32(wrap_f32(1. / 8.), 1. / 8.));
+        assert!(eq_f32(wrap_f32(1. / 2.), 1. / 2.));
+        assert!(eq_f32(wrap_f32(1.), 0.));
+    }
+
+    #[test]
+    fn it_eqs() {
+        assert!(eq_f32(0., 0.));
+        assert!(!eq_f32(0., f32::NAN));
+        assert!(!eq_f32(0., 1.));
+        assert!(eq_f32(1., 1.));
     }
 }
